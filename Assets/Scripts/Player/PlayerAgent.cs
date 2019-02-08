5bca394b6f45fcfdbc1ecedfa3f56121a4745160
opyr <bradt@ucsc.edu>
2019-02-07 19:44:18 -0800

    Starter Files
    Taken directly from James' Unity Project folder

---
+++ Assets/Scripts/Player/PlayerAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAgent : MonoBehaviour
+{
+	private string action;
+	private string state;
+	public float reward;
+	
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		Debug.Log("aa");
+    }
+
+	float QFuction()
+	{
+		return 0f;
+	}
+}
